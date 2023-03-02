1d7043b8ac202d826ee031041647c30a2d3993fa
flame225 <andubuisi225@gmail.comm>
2023-03-02 14:14:53 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let express = require("express");
+let app = express();
+let httpServer = require("http").createServer(app);
+let io = require("socket.io")(httpServer);
+
+let connections = [];
+
+io.on("connect", (socket) => {
+  connections.push(socket);
+  console.log(`${socket.id} has connected`);
+
+  socket.on("propogate", (data) => {
+    connections.map((con) => {
+      if (con.id !== socket.id) {
+        con.emit("onpropogate", data);
+      }
+    });
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} is disconnected`);
+    connections = connections.filter((con) => con.id !== socket.id);
+  });
+});
+
+
+// render our file to the web
+app.use(express.static("public"));
+
+let PORT = process.env.PORT || 8080;
+httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
